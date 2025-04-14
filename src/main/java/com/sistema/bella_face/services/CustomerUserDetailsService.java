436837da2e0487d351bf932d65bf4ac302cb5566
Cauã <114314281+CauaGoulart@users.noreply.github.com>
2025-04-14 20:16:50 -0300

    Add files via upload

---
+++ src/main/java/com/sistema/bella_face/services/CustomerUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.sistema.bella_face.services;
+
+import com.sistema.bella_face.entities.Customer;
+import com.sistema.bella_face.repositories.CustomerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomerUserDetailsService implements UserDetailsService {
+
+    private final CustomerRepository customerRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Customer customer = customerRepository.findByLogin(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado: " + username));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(customer.getLogin())
+                .password(customer.getPassword())
+                .build();
+    }
+}
