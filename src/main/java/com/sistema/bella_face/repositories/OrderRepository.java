222ba3b3980be279cbbea70f3ddf23442ea321c4
Cauã <114314281+CauaGoulart@users.noreply.github.com>
2025-04-28 16:42:22 -0300

    Add files via upload

---
+++ src/main/java/com/sistema/bella_face/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.sistema.bella_face.repositories;
+
+import com.sistema.bella_face.entities.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Integer> {
+}
